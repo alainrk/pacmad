dfe9bb000034164de1182ab8bbfda571732cd635
Alain <alain.devcs@gmail.com>
2021-12-23 08:37:24 +0000

    ghost loading

--- game.go
+++ game.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 )
@@ -21,32 +23,29 @@ func NewGame() *Game {
 		panic(err)
 	}
 
-	frames := []pixel.Rect{}
 	shotSprites := []*pixel.Sprite{}
-	for x := shotSpritesheet.Bounds().Min.X; x < shotSpritesheet.Bounds().Max.X; x += 32 {
-		for y := shotSpritesheet.Bounds().Min.Y; y < shotSpritesheet.Bounds().Max.Y; y += 32 {
-			frames = append(frames, pixel.R(x, y, x+32, y+32))
-			shotSprites = append(shotSprites, pixel.NewSprite(shotSpritesheet, frames[len(frames)-1]))
+	for y := shotSpritesheet.Bounds().Min.Y; y < shotSpritesheet.Bounds().Max.Y; y += 32 {
+		for x := shotSpritesheet.Bounds().Min.X; x < shotSpritesheet.Bounds().Max.X; x += 32 {
+			frame := pixel.R(x, y, x+32, y+32)
+			shotSprites = append(shotSprites, pixel.NewSprite(shotSpritesheet, frame))
 		}
 	}
 
-	frames = []pixel.Rect{}
 	ghostSprites := []*pixel.Sprite{}
 
-	startX := ghostSpritesheet.Bounds().Max.X / 3 * 2
-	startY := ghostSpritesheet.Bounds().Max.Y / 16 * 4
-	endX := ghostSpritesheet.Bounds().Max.X
-	endY := ghostSpritesheet.Bounds().Max.Y
 	step := 16.0
+	startX := ghostSpritesheet.Bounds().Max.X/3*2 + 3
+	startY := 168.0
+	endX := startX + (step * 8)
+	endY := startY + step
 
-	// for x := ghostSpritesheet.Bounds().Min.X; x < ghostSpritesheet.Bounds().Max.X; x += 32 {
-	// 	for y := ghostSpritesheet.Bounds().Min.Y; y < ghostSpritesheet.Bounds().Max.Y; y += 32 {
-	for x := startX; x < endX; x += step {
-		for y := startY; y < endY; y += step {
-			frames = append(frames, pixel.R(x, y, x+step, y+step))
-			ghostSprites = append(ghostSprites, pixel.NewSprite(ghostSpritesheet, frames[len(frames)-1]))
+	for y := startY; y < endY; y += step {
+		for x := startX; x < endX; x += step {
+			frame := pixel.R(x, y, x+step, y+step)
+			ghostSprites = append(ghostSprites, pixel.NewSprite(ghostSpritesheet, frame))
 		}
 	}
+	fmt.Println(len(ghostSprites))
 
 	return &Game{
 		shotSprites:  shotSprites,
@@ -72,13 +71,13 @@ func (f *Game) AddShot(pos pixel.Vec) {
 func (f *Game) drawPacmanSpritesForTestJustRemoveThisFunction(win *pixelgl.Window) {
 	for i, ghost := range f.ghostSprites {
 		mat := pixel.IM
-		mat = mat.Moved(pixel.V(float64(i*32), float64(i*32)))
+		mat = mat.Moved(pixel.V(float64(i*16)+float64(i*2.0), 0))
 		ghost.Draw(win, mat)
 	}
 }
 
 func (f *Game) Draw(win *pixelgl.Window) {
-	f.drawPacmanSpritesForTestJustRemoveThisFunction(win)
+	// f.drawPacmanSpritesForTestJustRemoveThisFunction(win)
 
 	for _, shot := range f.shots {
 		shot.Draw(win)
